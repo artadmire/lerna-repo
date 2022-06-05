50373d65467e42ecde89c4c79b57d43714a21941
artadmire <artadmire@163.com>
2022-06-05 21:46:24 +0800

    feat: eslint

---
+++ packages/pkg1/lib/pkg1.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = pkg1;
+
+
+function pkg1() {
+  // TODO
+
+  console.log('pk1');
+
+  
+}
+
---
+++ packages/pkg2/lib/pkg2.js
@@ -0,0 +1,9 @@
+'use strict';
+const pkg1 = require('pkg1')
+
+module.exports = pkg2;
+
+pkg1()
+function pkg2() {
+    // TODO
+}
