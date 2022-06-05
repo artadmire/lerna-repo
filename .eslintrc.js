a8a1bd1ef708845822f738a6e7432e6bcae202fe
artadmire <artadmire@163.com>
2022-06-05 21:58:22 +0800

    feat: test eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,125 @@
 module.exports = {
-  'parser': '@typescript-eslint/parser',
-  'plugins': ['@typescript-eslint'],
-  'rules': {
-    'no-var': 'error',// 不能使用var声明变量
-    'no-extra-semi': 'error',
-    '@typescript-eslint/indent': ['error', 2],
-    'import/extensions': 'off',
-    'linebreak-style': [0, 'error', 'windows'],
-    'indent': ['error', 2, { SwitchCase: 1 }], // error类型，缩进2个空格
-    'space-before-function-paren': 0, // 在函数左括号的前面是否有空格
-    'eol-last': 0, // 不检测新文件末尾是否有空行
-    'semi': ['error', 'always'], // 在语句后面加分号
-    'quotes': ['error', 'single'],// 字符串使用单双引号,double,single
-    'no-console': ['error', { allow: ['log', 'warn'] }],// 允许使用console.log()
-    'arrow-parens': 0,
-    'no-new': 0,//允许使用 new 关键字
-    'comma-dangle': [2, 'never'], // 数组和对象键值对最后一个逗号， never参数：不能带末尾的逗号, always参数：必须带末尾的逗号，always-multiline多行模式必须带逗号，单行模式不能带逗号
-    'no-undef': 0
+  'extends': ['plugin:import/errors'],
+  'plugins': ['import'],
+  'env': {
+    'es6': true,
+    'node': true
   },
   'parserOptions': {
     'ecmaVersion': 6,
     'sourceType': 'module',
     'ecmaFeatures': {
-      'modules': true
+      'impliedStrict': true,
+      'objectLiteralDuplicateProperties': false
     }
+  },
+  'rules': {
+    'array-bracket-spacing': ['error', 'never'],
+
+    'camelcase': ['error', {
+      'properties': 'never'
+    }],
+
+    'comma-dangle': ['error', 'never'],
+
+    'curly': ['error', 'all'],
+
+    'eol-last': ['error'],
+
+    'indent': ['error', 2, {
+      'SwitchCase': 1
+    }],
+
+    'keyword-spacing': ['error'],
+
+    'max-len': ['error', {
+      'code': 180,
+      'ignoreComments': true,
+      'ignoreRegExpLiterals': true
+    }],
+
+    'no-else-return': ['error'],
+
+    'no-mixed-spaces-and-tabs': ['error'],
+
+    'no-multiple-empty-lines': ['error'],
+
+    'no-spaced-func': ['error'],
+
+    'no-trailing-spaces': ['error'],
+
+    'no-undef': ['error'],
+
+    'no-unexpected-multiline': ['error'],
+
+    'no-unused-vars': ['error', {
+      'args': 'none',
+      'vars': 'all'
+    }],
+
+    'quotes': ['error', 'single', {
+      'allowTemplateLiterals': true,
+      'avoidEscape': true
+    }],
+
+    'semi': ['error', 'never'],
+
+    'space-before-blocks': ['error', 'always'],
+
+    'space-before-function-paren': ['error', 'never'],
+
+    'space-in-parens': ['error', 'never'],
+
+    'space-unary-ops': ['error', {
+      'nonwords': false,
+      'overrides': {}
+    }],
+
+    // 'valid-jsdoc': ['error']
+
+    // ECMAScript 6 rules
+
+    'arrow-body-style': ['error', 'as-needed', {
+      'requireReturnForObjectLiteral': false
+    }],
+
+    'arrow-parens': ['error', 'always'],
+
+    'arrow-spacing': ['error', {
+      'after': true,
+      'before': true
+    }],
+
+    'no-class-assign': ['error'],
+
+    'no-const-assign': ['error'],
+
+    'no-dupe-class-members': ['error'],
+
+    'no-duplicate-imports': ['error'],
+
+    'no-new-symbol': ['error'],
+
+    'no-useless-rename': ['error'],
+
+    'no-var': ['error'],
+
+    'object-shorthand': ['error', 'always', {
+      'avoidQuotes': true,
+      'ignoreConstructors': false
+    }],
+
+    'prefer-arrow-callback': ['error', {
+      'allowNamedFunctions': false,
+      'allowUnboundThis': true
+    }],
+
+    'prefer-const': ['error'],
+
+    'prefer-rest-params': ['error'],
+
+    'prefer-template': ['error'],
+
+    'template-curly-spacing': ['error', 'never']
   }
-};
+}
